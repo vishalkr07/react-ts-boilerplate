a2e6f49b549a65e5b6ae44dc3977735577f94f83
Vishal Kumar <vishalkr.k07@gmail.com>
2021-06-01 01:29:18 +0530

    select free port dynamically

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,46 +1,56 @@
-const path = require("path");
-const HTMLWebPackPlugin = require("html-webpack-plugin");
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+import path from "path";
+import HTMLWebPackPlugin from "html-webpack-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import findFreePorts from "find-free-ports";
 
-module.exports = {
-  mode: "development",
-  target: "web",
-  entry: "./src/index.tsx",
-  output: {
-    pathinfo: true,
-    filename: "[hash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"],
-  },
-  plugins: [
-    new HTMLWebPackPlugin({
-      template: "src/index.html",
-    }),
-    new ReactRefreshWebpackPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react",
-              "@babel/preset-typescript",
-            ],
-            plugins: [require.resolve("react-refresh/babel")],
+const getWebpackConfig = async () => {
+  const [port] = await findFreePorts(1, { startPort: 3000 });
+  return {
+    mode: "development",
+    target: "web",
+    entry: "./src/index.tsx",
+    output: {
+      pathinfo: true,
+      filename: "[hash].js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"],
+    },
+    plugins: [
+      new HTMLWebPackPlugin({
+        template: "src/index.html",
+      }),
+      new ReactRefreshWebpackPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+              plugins: [require.resolve("react-refresh/babel")],
+            },
           },
         },
-      },
-    ],
-  },
-  devServer: {
-    contentBase: [path.join(__dirname, "assets")],
-    hotOnly: true,
-  },
+      ],
+    },
+    devServer: {
+      contentBase: [path.join(__dirname, "assets")],
+      hotOnly: true,
+      port,
+      open: true,
+    },
+  };
+};
+
+export default () => {
+  return getWebpackConfig();
 };
